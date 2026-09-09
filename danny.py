2c999acfc4c9c17710b83d864da3bd259ed24e71
dhanushjettyhn <danu39955@gmail.com>
2026-09-09 14:52:20 +0530

    Update danny.py

--- danny.py
+++ danny.py
@@ -5,5 +5,5 @@ my_list = [12, 65, 54, 39, 102, 339, 221,]
 result = list(filter(lambda x: (x % 13 == 0), my_list))
 
 # display the result
-print("Numbers divisible by 13 are",result)
+print("Numbers divisible by 13 are",result)\\\\\\\\[]][]
 Run Code
